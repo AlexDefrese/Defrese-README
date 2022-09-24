8f628fec4fd546c98113f0ad5ddbb136d4c986c4
Alex Defrese <aidandefrese@gmail.com>
2022-09-24 14:48:06 -0400

    created and array of questions

--- index.js
+++ index.js
@@ -1,7 +1,70 @@
 // TODO: Include packages needed for this application
+const inquirer = require ('inquirer');
+const fs = require ('fs');
+const { type } = require('os');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+   inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+            validate: (titleInput)=> { if(titleInput){return true}else {return 'Please enter a project title to proceed.'}},
+        },
+        {
+            type: 'input',
+            message: 'Please describe your project:',
+            name: 'description',
+            validate: (descriptionInput)=> { if(descriptionInput){return true}else {return 'Please describe your project to proceed.'}},
+        },
+        {
+            type: 'input',
+            message: 'How do you install your project?',
+            name: 'install',
+            validate: (installInput)=> { if(installInput){return true}else {return 'Please provide installation instructions to proceed.'}},
+        },
+        {
+            type: 'input',
+            message: 'How do you you use your project?',
+            name: 'usage',
+            validate: (usageInput)=> { if(usageInput){return true}else {return 'Please provide instructions on how to use your project to proceed.'}},
+        },
+        {
+            type: 'list',
+            message: 'Which license applies:',
+            name: 'license',
+            choices: ['MIT', 'GPL', 'Apache', 'GNU', 'N/A'],
+            validate: (licenseInput)=> { if(licenseInput){return true}else {return 'Please provide a license for your project to proceed.'}},
+        },
+        {
+            type:'input',
+            message: 'Who contributed to this project?',
+            name: 'contributors',
+            validate: (contributeInput)=> { if(contributeInput){return true}else {return 'Please provide the names of the contributors for your project to proceed.'}},
+        },
+        {
+            type: 'input',
+            message: 'How should your project be tested?',
+            name: 'testing',
+            validate: (testInput)=> { if(testInput){return true}else {return 'Please provide a way to test your project to proceed.'}},
+        },
+        {
+            type: 'input',
+            message: 'Please provide your GitHub username:',
+            name: 'github',
+            validate: (githubInput)=> { if(githubInput){return true}else {return 'Please provide your GitHub username to proceed.'}},
+        },
+        {
+            type: 'input',
+            message: 'Please provide an email address you can be reached at if there are any questions:',
+            name: 'email',
+            validate: (emailInput)=> { if(emailInput){return true}else {return 'Please provide an email address to proceed.'}},
+        }
+
+    ])
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
